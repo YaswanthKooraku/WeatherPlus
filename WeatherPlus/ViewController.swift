ccef71abaefcac4c5e17e660aa24a4f4323ec254
YaswanthKooraku <veerayk@gmail.com>
2017-12-19 05:52:06 -0500

    Brand new project

---
+++ WeatherPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherPlus
+//
+//  Created by Yaswanth on 2017-12-19.
+//  Copyright © 2017 Yaswanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
